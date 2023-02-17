52fc1ca75b2b66ad4b1d36e62d34bf74f8dd65cb
andreacebrianbabel <andrea.cebrian@babelgroup.com>
2023-02-17 14:33:16 +0100

    pc00 estilos generales generados

--- src/app/shared/public-shared/public-shared.module.ts
+++ src/app/shared/public-shared/public-shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ 
+    CardProductComponent,
+   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
+  ],
+  exports: [
+    CardProductComponent
   ]
 })
 export class PublicSharedModule { }
