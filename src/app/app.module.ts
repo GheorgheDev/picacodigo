d6fc5154a3864252310a4487b4870da8fe708f6d
juanjo <juan.gsevilla@babelgroup.com>
2023-02-20 16:02:19 +0100

    "falta logo"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
