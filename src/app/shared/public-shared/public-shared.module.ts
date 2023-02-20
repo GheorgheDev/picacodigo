ac354c925564905402d2600a024145b9aad562ea
Barbaraarv <barbaraarv98@gmail.com>
2023-02-20 16:57:56 +0100

    Funcionan las tarjetas, no funciona el MAR

--- src/app/shared/public-shared/public-shared.module.ts
+++ src/app/shared/public-shared/public-shared.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardProductComponent } from './components/card-product/card-product.component';
 import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [ 
-    CardProductComponent,
    ],
   imports: [
     CommonModule,
     MatCardModule
   ],
   exports: [
-    CardProductComponent
   ]
 })
 export class PublicSharedModule { }
