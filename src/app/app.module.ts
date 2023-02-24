1d8f4b77685f01ae3c15362cb421a45b5d29a7b9
salvador.santos <salvador.santos@babelgroup.com>
2023-02-24 10:00:07 +0100

    pc-37-arreglos para dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,49 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PrivateModule } from './private/private.module';
 import { PublicModule } from './public/public.module';
-import { PrivateSharedModule } from './shared/private-shared/private-shared.module';
-import { PublicSharedModule } from './shared/public-shared/public-shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonHeaderComponent } from './shared/public-shared/header/common-header/common-header.component';
-import { SearchBarComponent } from './shared/public-shared/header/search-bar/search-bar.component';
-import { FormsModule } from '@angular/forms';
-import { PrivateHeaderComponent } from './shared/public-shared/header/private-header/private-header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatInputModule } from '@angular/material/input';
-import { PublicHeaderComponent } from './shared/public-shared/header/public-header/public-header.component';
-import { MatCardModule } from '@angular/material/card';
-import { MyProfilePrivComponent } from './private/my-profile-priv/my-profile-priv.component';
-import { MyProfilePublComponent } from './public/my-profile-publ/my-profile-publ.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CommonHeaderComponent,
-    SearchBarComponent,
-    PrivateHeaderComponent,
-    PublicHeaderComponent,
-    MyProfilePrivComponent,
-    MyProfilePublComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PublicModule,
     PrivateModule,
-    PrivateSharedModule,
-    PublicSharedModule,
+    SharedModule,
     BrowserAnimationsModule,
-    FormsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatInputModule,
-    MatCardModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,31 @@
+import { HeaderModule } from './components/header/header.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PublicSharedModule } from './public-shared/public-shared.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardProductComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
-    PublicSharedModule
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    HeaderModule,
+  ],
+  exports: [
+    CardProductComponent,
+    FooterComponent,
   ]
 })
 export class SharedModule { }
