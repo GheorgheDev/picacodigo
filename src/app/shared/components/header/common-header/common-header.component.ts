a05a69e120b54444ec4427732c1018ddce95ec1a
Barbaraarv <barbaraarv98@gmail.com>
2023-03-14 17:06:36 +0100

    aparece menu burger al pasar a formato tablet pc-34

--- src/app/shared/components/header/common-header/common-header.component.ts
+++ src/app/shared/components/header/common-header/common-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-common-header',
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./common-header.component.scss'],
 })
 export class CommonHeaderComponent implements OnInit {
+
+  userIsLogged:boolean = false;
+
+  burgerMenuToggle: string = 'off';
+  ScreenWidth:number
+
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.onWindowResize();
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.ScreenWidth = window.innerWidth;
+    if (this.ScreenWidth < 769) {
+      this.burgerMenuToggle = 'on';
+    } else {
+      this.burgerMenuToggle = 'off';
+    }
+  }
 }
--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -3,22 +3,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrivateHeaderComponent } from './private-header/private-header.component';
 import { PublicHeaderComponent } from './public-header/public-header.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     PrivateHeaderComponent,
     PublicHeaderComponent,
-    SearchBarComponent,
     CommonHeaderComponent,
   ],
-  imports: [CommonModule, MatToolbarModule, MatMenuModule],
+  imports: [
+    CommonModule, 
+    MatToolbarModule, 
+    MatMenuModule,
+    MatIconModule
+  ],
   exports: [
     PrivateHeaderComponent,
     PublicHeaderComponent,
-    SearchBarComponent,
     CommonHeaderComponent,
   ],
 })
