620c42bab1d6c76f99eec8cf74f58fdc0621501e
andreacebrianbabel <andrea.cebrian@babelgroup.com>
2023-03-27 13:34:31 +0200

    pc52 modulo auth completado

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,33 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: [ './login.component.scss']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
   public formSubmitted = false;
 
   public loginForm = this.fb.group({
-    email: ['test100@hotmail.com', [Validators.required, Validators.email ]],
-    password: ['123456', Validators.required ],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
 
   });
 
-  constructor( private router: Router,
-               private fb: FormBuilder ) {}
-
- 
+  constructor(private fb: FormBuilder) { }
 
   login() {
-
-    /* this.router.navigateByUrl('/'); */
-
-    console.log( this.loginForm.value )
-
+    console.log(this.loginForm.value)
   }
-
 }
