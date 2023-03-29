fbfbe47bfd6b7b59adb1609489aed110cd8d570d
andreacebrianbabel <andrea.cebrian@babelgroup.com>
2023-03-29 12:25:06 +0200

    pc55 modularizacion auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,27 +1,17 @@
+import { LoginModule } from './login/login.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-
-
+import { RegisterModule } from './register/register.module';
 
 @NgModule({
   declarations: [
-    LoginComponent,
-    RegisterComponent,
-  ],
-  exports: [
-    LoginComponent,
-    RegisterComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule
+    LoginModule,
+    RegisterModule
+  ],
+  exports: [
   ]
 })
 export class AuthModule { }
