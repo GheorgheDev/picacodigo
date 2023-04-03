4612fe5be3217e9064df6353ea96627c4b09406e
Gheorghe Bucurici <gheorghe.bucurici@babelgroup.com>
2023-04-03 15:13:49 +0200

    Añadir todos los servicios de notificaciones

--- src/app/private/components/notificaciones/notificaciones.module.ts
+++ src/app/private/components/notificaciones/notificaciones.module.ts
@@ -5,21 +5,22 @@ import { CommonModule } from '@angular/common';
 import { NotificationPageComponent } from './components/notification-page/notification-page.component';
 import { NotificationsPanelComponent } from './components/notifications-panel/notifications-panel.component';
 
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MailBoxPanelComponent } from './components/mail-box-panel/mail-box-panel.component';
 import { SendMailPanelComponent } from './components/send-mail-panel/send-mail-panel.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {TextFieldModule} from '@angular/cdk/text-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { TextFieldModule } from '@angular/cdk/text-field';
 import { InvalidFormDialogComponent } from './components/invalid-form-dialog/invalid-form-dialog.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { ValidMessageformDialogComponent } from './components/valid-messageform-dialog/valid-messageform-dialog.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     MatDialogModule,
     SharedModule,
     HeaderModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule
   ],
   exports: [
     NotificationPageComponent,
--- src/app/private/components/notificaciones/services/notification-service.service.ts
+++ src/app/private/components/notificaciones/services/notification-service.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { UserData } from 'src/app/shared/models/user-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllMessagesByUserId(idUser: string): Observable<any> {
+    return this.http.get<any>(`/api/messages/get/all/${idUser}`)
+      .pipe(
+        map((res: any) => res)
+      )
+  }
+
+  markMessageAsRead(idMessage: any): Observable<any> {
+    return this.http.patch(`/api/messages/update/message/read`, { idMessage })
+  }
+
+  deleteMessage(idMessage: any): Observable<any> {
+    return this.http.delete(`/api/messages/delete/message/${idMessage}`);
+  }
 }
